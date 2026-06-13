85eec9cd990f4da751c2fb4a3bb4969ae0d123e1
venkatbangla-boop <venkatbangla@gmail.com>
2026-06-14 01:15:46 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "sri-sashti-v24-final";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/css/styles.css",
+  "./assets/js/app.js",
+  "./manifest.webmanifest",
+  "./assets/icons/logo-transparent.png",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
